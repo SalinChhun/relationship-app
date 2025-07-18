855379edc5ebb492c7ea8f66feeff00a95fd066b
SalinChhun <salinchhun123079@gmail.com>
2025-07-18 15:25:15 +0700

    fix sw.js file

--- public/sw.js
+++ public/sw.js
@@ -87,3 +87,23 @@ define(['./workbox-7144475a'], (function (workbox) { 'use strict';
   self.__WB_DISABLE_DEV_LOGS = true;
 
 }));
+
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || "New Notification";
+  const options = {
+    body: data.body || "",
+    icon: "/icon-192x192.png", // adjust as needed
+    data: data.url || "/"
+  };
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data)
+  );
+});
